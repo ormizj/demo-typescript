e972b0cc9630cfc84eb5e3ccdb80db90751c75b7
spiderpig60 <spiderpig60@gmail.com>
2024-08-08 18:52:59 +0300

    abstract class example

--- typescript/abstract.ts
+++ typescript/abstract.ts
@@ -1,32 +1,44 @@
 interface MyInterface {
-    name:string;
-    age:number;
+	name: string;
+	age: number;
 
-    getName: () => string;
+	getName: () => string;
 }
 
 abstract class MyAbstract {
-    abstract name:string;
-    age:number;
+	abstract name: string;
+	age: number;
 
-    abstract getName():string
+	constructor(age: number) {
+		this.age = age;
+	}
+
+	abstract getName(): string;
 }
 
-
-
 class MyInterfaceClass implements MyInterface {
-    name: string;
-    age: number;
+	name: string;
+	age: number;
 
-    getName() {
-        return this.name
-    };
+	constructor(name: string, age: number) {
+		this.name = name;
+		this.age = age;
+	}
+
+	getName() {
+		return this.name;
+	}
 }
 
 class MyAbstractClass extends MyAbstract {
-    name: string;
+	name: string;
 
-    getName(): string {
-        return this.name
-    }
+	constructor(name: string, age: number) {
+		super(age);
+		this.name = name;
+	}
+
+	getName(): string {
+		return this.name;
+	}
 }
